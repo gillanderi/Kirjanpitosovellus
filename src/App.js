01d5be3798191fc1c27e3b6d52e6ae924851093e
Rosa Argillander <droar003@edu.xamk.fim>
2022-04-25 19:31:10 +0300

    valmis versio jaettavaksi

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+
+function App() {
+
+
+
+
+
+  return (
+
+    <div className="App">
+      <header>
+
+      <h1>Velat</h1>
+
+      </header>
+      <div className="velka_taulukko">
+
+        <div className="otsikot_taulukossa">
+
+        </div>
+        <div className="sarakkeet">
+          
+        </div>
+      <div className="lisaa_taulukkoon">
+
+      </div>
+      
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
